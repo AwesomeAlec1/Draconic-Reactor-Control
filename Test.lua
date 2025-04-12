053f5f4a2b8dac3f39d024f04e8c637071ba44ec
James <68837571+BrokenSynapse@users.noreply.github.com>
2025-04-11 22:00:54 -0400

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -15,6 +15,7 @@ if updateVariable == "y" then
 	local installPath = shell.resolve(process.info().path)
 	os.execute("wget -f \"https://raw.githubusercontent.com/AwesomeAlec1/Draconic-Reactor-Control/refs/heads/Installer/DraconicInstaller.lua\" " .. installPath)
 	os.sleep(1)
+	os.execute("cls")
 	os.execute(installPath)
 end
 
