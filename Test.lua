643f6ede42e639ba7e4d9c4bed2325ae1a297340
James <68837571+BrokenSynapse@users.noreply.github.com>
2025-04-11 22:14:09 -0400

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -14,8 +14,7 @@ local updateVariable = tostring(io.read())
 if updateVariable == "y" then
 	local installPath = shell.resolve(process.info().path)
 	os.execute("wget -f \"https://raw.githubusercontent.com/AwesomeAlec1/Draconic-Reactor-Control/refs/heads/Installer/Test.lua\" " .. installPath)
-	os.execute(installPath)
-	os.exit()
+	os.execute("exit", installPath)
 end
 
 local function locateAndInstall(link, fileName)
