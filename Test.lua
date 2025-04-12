8076fcb43dc7e10538b49896965fa30263f1e09f
James <68837571+BrokenSynapse@users.noreply.github.com>
2025-04-11 22:06:55 -0400

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -13,7 +13,7 @@ print("Check for update? (y/n)")
 local updateVariable = tostring(io.read())
 if updateVariable == "y" then
 	local installPath = shell.resolve(process.info().path)
-	os.execute("wget -f \"https://raw.githubusercontent.com/AwesomeAlec1/Draconic-Reactor-Control/refs/heads/Installer/DraconicInstaller.lua\" " .. installPath)
+	os.execute("wget -f \"https://raw.githubusercontent.com/AwesomeAlec1/Draconic-Reactor-Control/refs/heads/Installer/Test.lua\" " .. installPath)
 	print("Update finished, please relaunch.")
 	os.exit(0)
 end
